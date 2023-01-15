dd4b8d1c85e04c18437f616a3464197bafb8aea7
Akash-M1 <83898316+Akash-M1@users.noreply.github.com>
2023-01-15 15:04:22 +0530

    Final Push

--- src/controllers/products_controllers.js
+++ src/controllers/products_controllers.js
@@ -1,5 +1,6 @@
 const products = require('../models/products');
 
+//Controller for creating and pushing the product inside the database
 exports.createProduct = (req,res)=>{
     products.create(req.body,(err,data)=>{
         if(err){
@@ -18,7 +19,7 @@ exports.createProduct = (req,res)=>{
 }
 
 
-
+//Controller for getting all the products listed in the Database
 exports.getAllProducts = (req,res)=>{
     products.find().then((data)=>{
         res.status(200).json({
@@ -29,7 +30,7 @@ exports.getAllProducts = (req,res)=>{
 }
 
 
-
+//Controller for Updating the product with the given ID
 exports.updateProductByProductId = (req,res)=>{
     const updateableValue = Object.keys(req.body).slice(1).reduce((result, key) => {
         result[key] = req.body[key];
@@ -49,6 +50,7 @@ exports.updateProductByProductId = (req,res)=>{
     })
 }
 
+//Controller for Deleting the product with the given ID
 exports.deleteProductByProductId = (req,res)=>{
     products.deleteOne(req.body).then((data)=>{
         res.status(200).json({
@@ -64,6 +66,7 @@ exports.deleteProductByProductId = (req,res)=>{
     ])
 }
 
+//Controller for Fetching all the Featured products
 exports.getFeaturedProducts = (req,res)=>{
     products.find({"featured":true}).then((data)=>{
         res.status(200).json({
@@ -79,6 +82,7 @@ exports.getFeaturedProducts = (req,res)=>{
     })
 }
 
+//Controller for fetching the products after applying the certain price filter in database
 exports.getProductsAfterPriceFilter = (req,res)=>{
     products.find({"price":{"$lt":req.body.hcost}}).then((data)=>{
         res.status(200).json({
@@ -94,6 +98,7 @@ exports.getProductsAfterPriceFilter = (req,res)=>{
     })
 }
 
+//Controller for fetching the products after applying the certain rating filter in database
 exports.getProductsAfterRatingFilter = (req,res)=>{
     products.find({"rating":{"$gt":req.body.lrating}}).then((data)=>{
         res.status(200).json({
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//Importing All the required Frameworks and Libraries
 const express = require('express');
 const cors = require('cors');
 const bodyParser=require('body-parser');
@@ -6,11 +7,12 @@ const dotenv = require('dotenv');
 const app=express();
 dotenv.config();
 
+//Importing all the required Files and Modules
 const routes = require("./routes/index");
 const db = require('./lib/db');
 
 
-
+//Using Different middelwares
 const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -18,8 +20,7 @@ app.use(bodyParser.json());
 
 app.use('/',routes);
 
-console.log("Everything Happend successfully")
-
+//Listening to the requests from the users at the given port
 app.listen(PORT,(err)=>{
     if(err){
         console.log("Error in listening");
